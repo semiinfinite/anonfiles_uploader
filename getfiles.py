e2374b5890361bd5495582212eae82687460b267
Michael Mitchell <imnotmitchell@gmail.com>
2012-09-22 16:05:04 -0400

    removed unnecessary files

--- getfiles.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for dirname, dirnames, filenames in os.walk('roms'):
-#    for subdirname in dirnames:
-#        print os.path.join(dirname, subdirname)
-    for filename in filenames:
-        print os.path.join(dirname, filename)
